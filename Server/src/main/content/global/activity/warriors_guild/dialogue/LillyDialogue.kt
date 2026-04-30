ad1579e370904e60fb725dd32aa7965d5d98ae72
szumaster3 <szumaster2009@protonmail.com>
2026-05-01 01:14:56 +0200

    Ref & Lily dialogue update

--- Server/src/main/content/global/activity/warriors_guild/dialogue/LillyDialogue.kt
+++ Server/src/main/content/global/activity/warriors_guild/dialogue/LillyDialogue.kt
@@ -1,9 +1,9 @@
 package content.global.activity.warriors_guild.dialogue
 
 import core.api.openNpcShop
-import core.api.sendOptions
 import core.game.dialogue.Dialogue
 import core.game.dialogue.FaceAnim
+import core.game.dialogue.Topic
 import core.game.node.entity.npc.NPC
 import core.game.node.entity.player.Player
 import core.plugin.Initializable
@@ -26,34 +26,15 @@ class LillyDialogue(player: Player? = null) : Dialogue(player) {
         when (stage) {
             0 -> player(FaceAnim.HALF_GUILTY, "Umm... do you sell potions?").also { stage++ }
             1 -> npc(FaceAnim.HALF_GUILTY, "Erm... yes. When I'm not drinking them.").also { stage++ }
-            2 ->
-                sendOptions(
-                    player,
-                    "What would you like to say?",
-                    "I'd like to see what you have for sale.",
-                    "That's a pretty wall hanging.",
-                    "Bye!",
-                ).also {
-                    stage++
-                }
-            3 ->
-                when (buttonId) {
-                    1 -> player(FaceAnim.HALF_GUILTY, "I'd like to see what you have for sale.").also { stage++ }
-                    2 -> player(FaceAnim.HALF_GUILTY, "That's a pretty wall hanging.").also { stage = 5 }
-                    3 -> player(FaceAnim.HALF_GUILTY, "Bye!").also { stage = 8 }
-                }
-            4 -> {
-                end()
-                openNpcShop(player, npc.id)
-            }
-            5 -> npc(FaceAnim.HALF_GUILTY, "Do you think so? I made it my self.").also { stage++ }
-            6 ->
-                player(
-                    FaceAnim.HALF_GUILTY,
-                    "Really? Is that why there's all this cloth and dye around?",
-                ).also { stage++ }
-            7 -> npc(FaceAnim.HALF_GUILTY, "Yes, it's a hobby of mine when I'm.... relaxing.").also { stage++ }
-            8 -> npc(FaceAnim.HALF_GUILTY, "Have fun and come back soon!").also { stage = END_DIALOGUE }
+            2 -> showTopics(
+                Topic("I'd like to see what you have for sale.",openNpcShop(player, npc.id)),
+                Topic("That's a pretty wall hanging.", 3),
+                Topic("Bye!", 6)
+            )
+            3 -> npc(FaceAnim.HALF_GUILTY, "Do you think so? I made it my self.").also { stage++ }
+            4 -> player(FaceAnim.HALF_GUILTY, "Really? Is that why there's all this cloth and dye around?").also { stage++ }
+            5 -> npc(FaceAnim.HALF_GUILTY, "Yes, it's a hobby of mine when I'm.... relaxing.").also { stage++ }
+            6 -> npc(FaceAnim.HALF_GUILTY, "Have fun and come back soon!").also { stage = END_DIALOGUE }
         }
         return true
     }
