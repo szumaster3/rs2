9dfd9454f5c654ebdaa1454d867cce63a19bef76
szumaster3 <szumaster2009@protonmail.com>
2026-06-21 04:11:56 +0200

    Fix DisplayModel packet

--- Server/src/main/core/net/packet/out/DisplayModel.kt
+++ Server/src/main/core/net/packet/out/DisplayModel.kt
@@ -27,8 +27,9 @@ class DisplayModel : OutgoingPacket<DisplayModelContext> {
             }
 
             DisplayModelContext.ModelType.ITEM -> {
+                val value = if (context.amount > 0) context.amount else context.zoom
                 buffer = IoBuffer(Network.DISPLAY_MODEL_ITEM)
-                buffer.putInt(context.zoom)
+                buffer.putInt(value)
                 buffer.putIntB((context.interfaceId shl 16) or context.childId)
                 buffer.putLEShortA(context.nodeId)
                 buffer.putLEShort(context.player.interfaceManager.getPacketCount(1))
